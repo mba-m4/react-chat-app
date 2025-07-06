0a50be1bd190997d835700e68da9c5e943d00f5d
mba-m4 <rsfakqj10@icloud.com>
2025-07-07 02:43:31 +0900

    Revert "add(frontend): Add Chat component base"

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
-import Chat from "./components/Chat/Chat"
+import { Button } from "@/components/ui/button"
 
 function App() {
   return (
-    <>
-    <Chat />
-    </>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
   )
 }
 
--- frontend/src/components/Chat/components/ChatHeader.tsx
+++
@@ -1,9 +0,0 @@
-function ChatHeader() {
-  return (
-    <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight bg-gray-100">
-      React Chat App
-    </h3>
-  )
-}
-
-export default ChatHeader
