e327fc4a5349e2792249291572ac6083c711139b
mba-m4 <rsfakqj10@icloud.com>
2025-07-07 03:39:54 +0900

    add(frontend): Create ChatMessageInput component with basic input functionality

--- frontend/src/components/Chat/Chat.tsx
+++ frontend/src/components/Chat/Chat.tsx
@@ -1,11 +1,17 @@
 import ChatHeader from "./components/ChatHeader"
+import ChatMessageInput from "./components/ChatMessageInput"
 import ChatMessagesList from "./components/ChatMessagesList"
 
 function Chat() {
+  const handleSend = (text:string) => {
+    console.log(text)
+  }
+
   return (
     <>
     <ChatHeader />
     <ChatMessagesList />
+    <ChatMessageInput onSend={handleSend}/>
     </>
   )
 }
